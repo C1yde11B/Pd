2f3e503a5fe71516dfde92032543e76a5ea7564e
C1yde11B <Clydellb0@gmail.com>
2024-10-31 08:38:03 -0500

    Initial Commit

---
+++ Pd/PdApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdApp.swift
+//  Pd
+//
+//  Created by AM Student on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct PdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PdTests/PdTests.swift
@@ -0,0 +1,17 @@
+//
+//  PdTests.swift
+//  PdTests
+//
+//  Created by AM Student on 10/31/24.
+//
+
+import Testing
+@testable import Pd
+
+struct PdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
